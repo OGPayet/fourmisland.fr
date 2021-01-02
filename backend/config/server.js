7edd68a34f27a592b6a3ad4aa2de1cb6f11262f0
Pierre Payet <pierre.payet@outlook.com>
2021-01-02 11:08:08 +0100

    :rocket: setup project

---
+++ backend/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'aa1c8a766f9280073a5cdd454ea49797'),
+    },
+  },
+});
---
+++ backend/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '2f5e2ff9-931d-48ea-aa23-995851cf1c60'
+};
